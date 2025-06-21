b38526e9223af343a9ab56c48316f6f530c466cc
fabri236 <fabrizio_m@hotmail.com>
2025-06-21 17:14:56 -0300

    batch inversion

---
+++ ec-divisors-contest/ec-divisors-contest-src/src/inversion.rs
@@ -0,0 +1,68 @@
+use ff::Field;
+
+pub struct BatchInverse;
+
+impl BatchInverse {
+    fn accumulate<'a, F, I>(mut elems: I, space: Option<Vec<F>>) -> Vec<F>
+    where
+        F: Field,
+        I: Iterator<Item = &'a F>,
+    {
+        let mut products = space.unwrap_or_else(|| Vec::with_capacity(256));
+        products.truncate(0);
+        let mut acc = F::ONE;
+        while let Some(elem) = elems.next() {
+            acc *= *elem;
+            products.push(acc);
+        }
+        assert_ne!(acc, F::ZERO, "tried to batch invert a 0");
+        products
+    }
+
+    fn invert<'a, F, I>(mut elems: I, products: &[F])
+    where
+        F: Field,
+        I: Iterator<Item = &'a mut F>,
+    {
+        let mut all_inverses = products.last().unwrap().invert().unwrap();
+        let mut products = products.iter().rev().skip(1);
+
+        loop {
+            let elem = elems.next();
+            let product = products.next();
+
+            match (elem, product) {
+                (Some(elem), Some(product)) => {
+                    let new_elem: F = all_inverses * product;
+                    all_inverses.mul_assign(&*elem);
+                    *elem = new_elem;
+                }
+                (Some(elem), None) => {
+                    *elem = all_inverses;
+                    break;
+                }
+                _ => unreachable!(),
+            }
+        }
+    }
+
+    pub fn invert_slice<F: Field>(slice: &mut [F]) {
+        let space: Vec<F> = Vec::with_capacity(slice.len());
+        let elems = slice.iter();
+        let products = Self::accumulate(elems, Some(space));
+        let elems = slice.iter_mut().rev();
+        Self::invert(elems, &products);
+    }
+}
+
+#[test]
+fn inversion() {
+    use pasta_curves::Fp;
+    let vals = [2, 5, 25, 235, 23, 324, 432, 4_u64];
+    let vals = vals.map(Fp::from);
+    let mut inverses = vals.clone();
+    BatchInverse::invert_slice(&mut inverses);
+    for i in 0..vals.len() {
+        assert_eq!(vals[i] * inverses[i], Fp::ONE);
+    }
+}
