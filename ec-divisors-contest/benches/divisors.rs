d48870b61354158308148812b8d2661471ce508e
fabri236 <fabrizio_m@hotmail.com>
2025-07-01 18:29:11 -0300

    cargo fmt

--- ec-divisors-contest/benches/divisors.rs
+++ ec-divisors-contest/benches/divisors.rs
@@ -1,6 +1,6 @@
 use ciphersuite::{
-    group::{ff::Field, Group},
-    Ciphersuite, Ed25519,
+  group::{ff::Field, Group},
+  Ciphersuite, Ed25519,
 };
 use dalek_ff_group::EdwardsPoint;
 use ec_divisors_contest::{check_init_contest, check_init_ref, run_bench_contest, run_bench_ref};
@@ -11,26 +11,26 @@ use criterion::{criterion_group, criterion_main, Criterion};
 // Benchmark the reference implementation and the implementation in
 // ../ec-divisors-contest-src
 fn bench_scalar_mul_divisors(c: &mut Criterion) {
-    let mut group = c.benchmark_group("ec-divisors");
-    group.measurement_time(core::time::Duration::from_secs(60));
+  let mut group = c.benchmark_group("ec-divisors");
+  group.measurement_time(core::time::Duration::from_secs(60));
 
-    let point = EdwardsPoint::generator();
-    let rand_scalar = <Ed25519 as Ciphersuite>::F::random(&mut OsRng);
+  let point = EdwardsPoint::generator();
+  let rand_scalar = <Ed25519 as Ciphersuite>::F::random(&mut OsRng);
 
-    check_init_ref(&point, &rand_scalar);
-    check_init_contest(&point, &rand_scalar);
+  check_init_ref(&point, &rand_scalar);
+  check_init_contest(&point, &rand_scalar);
 
-    // Run the benchmark for the reference implementation
-    group.bench_function("reference-impl", |b| {
-        b.iter_with_large_drop(|| run_bench_ref(&point, &rand_scalar))
-    });
+  // Run the benchmark for the reference implementation
+  group.bench_function("reference-impl", |b| {
+    b.iter_with_large_drop(|| run_bench_ref(&point, &rand_scalar))
+  });
 
-    // Run the benchmark for the contest implementation
-    group.bench_function("contest-impl", |b| {
-        b.iter_with_large_drop(|| run_bench_contest(&point, &rand_scalar))
-    });
+  // Run the benchmark for the contest implementation
+  group.bench_function("contest-impl", |b| {
+    b.iter_with_large_drop(|| run_bench_contest(&point, &rand_scalar))
+  });
 
-    group.finish();
+  group.finish();
 }
 
 criterion_group!(benches, bench_scalar_mul_divisors);
--- ec-divisors-contest/tests/divisors.rs
+++ ec-divisors-contest/tests/divisors.rs
@@ -1,8 +1,8 @@
 use ec_divisors_contest::{check_init_contest, check_init_ref, run_bench_contest, run_bench_ref};
 
 use ciphersuite::{
-    group::{ff::Field, Group},
-    Ciphersuite, Ed25519,
+  group::{ff::Field, Group},
+  Ciphersuite, Ed25519,
 };
 use dalek_ff_group::EdwardsPoint;
 use rand_core::OsRng;
@@ -11,24 +11,24 @@ use rand_core::OsRng;
 // result as the reference implementation
 #[test]
 fn divisors_contest_test() {
-    static N_ITERS: usize = 20;
+  static N_ITERS: usize = 20;
 
-    for i in 0..N_ITERS {
-        println!("Testing with random scalar {} / {}", i + 1, N_ITERS);
+  for i in 0 .. N_ITERS {
+    println!("Testing with random scalar {} / {}", i + 1, N_ITERS);
 
-        let point = EdwardsPoint::generator();
-        let scalar = <Ed25519 as Ciphersuite>::F::random(&mut OsRng);
+    let point = EdwardsPoint::generator();
+    let scalar = <Ed25519 as Ciphersuite>::F::random(&mut OsRng);
 
-        check_init_ref(&point, &scalar);
-        check_init_contest(&point, &scalar);
+    check_init_ref(&point, &scalar);
+    check_init_contest(&point, &scalar);
 
-        // Get divisors
-        let ref_res = run_bench_ref(&point, &scalar).0;
-        let res = run_bench_contest(&point, &scalar).0;
+    // Get divisors
+    let ref_res = run_bench_ref(&point, &scalar).0;
+    let res = run_bench_contest(&point, &scalar).0;
 
-        assert_eq!(ref_res.y_coefficients, res.y_coefficients);
-        assert_eq!(ref_res.yx_coefficients, res.yx_coefficients);
-        assert_eq!(ref_res.x_coefficients, res.x_coefficients);
-        assert_eq!(ref_res.zero_coefficient, res.zero_coefficient);
-    }
+    assert_eq!(ref_res.y_coefficients, res.y_coefficients);
+    assert_eq!(ref_res.yx_coefficients, res.yx_coefficients);
+    assert_eq!(ref_res.x_coefficients, res.x_coefficients);
+    assert_eq!(ref_res.zero_coefficient, res.zero_coefficient);
+  }
 }
